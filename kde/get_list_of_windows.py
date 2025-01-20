75ac2708f9edf4f03f68a438ca8c17dead5b031a
aniuzhong <zxy_9125@163.com>
2025-01-20 19:35:45 +0800

    Add scripts for getting list  of windows on KDE wayland

---
+++ kde/get_list_of_windows.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from datetime import datetime
+
+def get_list_of_windows():
+   datetime_now = datetime.now()
+
+   script = os.getcwd() + "/list_windows.js"
+
+   reg_script_number = subprocess.run("dbus-send --print-reply --dest=org.kde.KWin \
+                        /Scripting org.kde.kwin.Scripting.loadScript \
+                        string:" + script + " | awk 'END {print $2}'",
+                           capture_output=True, shell=True).stdout.decode().split("\n")[0]
+
+   subprocess.run("dbus-send --print-reply --dest=org.kde.KWin /Scripting/Script" + reg_script_number + " org.kde.kwin.Script.run",
+                  shell=True, stdout=subprocess.DEVNULL)
+   subprocess.run("dbus-send --print-reply --dest=org.kde.KWin /Scripting/Script" + reg_script_number + " org.kde.kwin.Script.stop",
+                  shell=True, stdout=subprocess.DEVNULL)  # unregister number
+
+   since = str(datetime_now)
+
+   msg = subprocess.run("journalctl _COMM=kwin_wayland -o cat --since \"" + since + "\"",
+                        capture_output=True, shell=True).stdout.decode().rstrip().split("\n")
+   msg = [el.lstrip("js: ") for el in msg]
+
+   return msg
+
+
+print('\n'.join(get_list_of_windows()))
